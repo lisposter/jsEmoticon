45aff708235505e766ebf87728d39f210ff40ea1
Leigh Zhu <i@zhuli.me>
2013-04-20 19:35:56 +0800

    fix regexp issue

--- jsemoticon.js
+++ jsemoticon.js
@@ -9,12 +9,12 @@
 		//all of the smilies' definition.
 		//smilies: the tag you will used in your text; filename: the smilies's filename.
 		emotions: [
-			{smilies: ':ali啊啊啊:', filename: 'ali_aaa.gif'},
-			{smilies: ':ali闭嘴:', filename: 'ali_bizui.gif'},
-			{smilies: ':ali不公平:', filename: 'ali_bugongpin.gif'},
-			{smilies: ':ali不耐烦:', filename: 'ali_bunaifan.gif'},
-			{smilies: ':ali嘲弄:', filename: 'ali_chaonong.gif'},
-			{smilies: ':ali大汗:', filename: 'ali_dahan.gif'}
+			{smilies: '[ali啊啊啊]', filename: 'ali_aaa.gif'},
+			{smilies: '[ali闭嘴]', filename: 'ali_bizui.gif'},
+			{smilies: '[ali不公平]', filename: 'ali_bugongpin.gif'},
+			{smilies: '[ali不耐烦]', filename: 'ali_bunaifan.gif'},
+			{smilies: '[ali嘲弄]', filename: 'ali_chaonong.gif'},
+			{smilies: '[ali大汗]', filename: 'ali_dahan.gif'}
 		]
 	}
 
@@ -26,7 +26,7 @@
 		var targetNode = document.getElementById(_cfg.domID);
 		var origString = targetNode.innerHTML;
 		
-		var modString = origString.replace(em.smilies, '<img src="' + _cfg.path + em.filename + '" />');
+		var modString = origString.replace(new RegExp(pattern, 'g'), '<img src="' + _cfg.path + em.filename + '" />');
 		targetNode.innerHTML = modString;
 	}
 	
